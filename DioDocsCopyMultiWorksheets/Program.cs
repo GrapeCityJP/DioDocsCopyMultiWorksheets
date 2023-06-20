6360b571bb3e76df7d67fb040e2d0afc36778c78
Kuni <kuni.ujiie@grapecity.com>
2023-06-20 10:40:59 +0900

    プロジェクト ファイルを追加します。

---
+++ DioDocsCopyMultiWorksheets/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("複数のワークシートをコピー");
+
+// ワークブックを開く
+var workbook = new GrapeCity.Documents.Excel.Workbook();
+workbook.Open("multiworksheet.xlsx");
+
+// Sheet1とSheet2を末尾にコピー
+workbook.Worksheets[new string[] { "Sheet1", "Sheet2" }].Copy();
+
+//// Sheet1とSheet2をSheet3の後にコピー
+//workbook.Worksheets[new string[] { "Sheet1", "Sheet2" }].CopyAfter(workbook.Worksheets[2]);
+
+//// Sheet1とSheet2をSheet1の前にコピー
+//workbook.Worksheets[new string[] { "Sheet1", "Sheet2" }].CopyBefore(workbook.Worksheets[0]);
+
+// Excelファイルとして保存
+workbook.Save("CopyMultipleWorksheets.xlsx");
